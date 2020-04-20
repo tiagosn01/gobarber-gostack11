4b07bc1ec221dfd65c5873f4396e1b0d41ae4b0a
tiagosn01 <tiagosilva0922@gmail.com>
2020-04-20 09:57:45 -0300

    Authenticate and services configs

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'afe047154ce69894c2a8e93d65dc3afb',
+    expiresIn: '7d',
+  },
+};
---
+++ backend/src/routes/users.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
+  return response.json({ ok: true });
+});
+
+export default usersRouter;
