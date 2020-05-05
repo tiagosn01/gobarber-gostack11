e26f52d289a42ffe3942fe12b39103d74163c143
tiagosn01 <tiagosilva0922@gmail.com>
2020-05-05 11:04:03 -0300

    Configs SignIn, SignUp and Authenticate context

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn';
+
+import GlobalStyle from './styles/global';
+
+import { AuthProvider } from './hooks/AuthContext';
+
+const App: React.FC = () => (
+  <>
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+
+    <GlobalStyle />
+  </>
+);
+
+export default App;
